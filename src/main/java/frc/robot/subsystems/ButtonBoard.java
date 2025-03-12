d889e5be5e47529ae833dac664a41807bdebacdf
Robert <77898045+remken07@users.noreply.github.com>
2025-03-12 13:06:56 -0400

    update/fix

--- src/main/java/frc/robot/subsystems/ButtonBoard.java
+++ src/main/java/frc/robot/subsystems/ButtonBoard.java
@@ -16,8 +16,8 @@ public class ButtonBoard {
     public ButtonBoard(CommandGenericHID m_buttonBoard, Arm arm, Wrist wrist, Effector effector) {
         this.buttonBoard = m_buttonBoard;
         
-        buttonBoard.button(0).onTrue(Commands.runOnce(this::toggleFunction)); // Toggle
-        buttonBoard.button(0).onChange(Commands.runOnce(this::toggleFunction));
+        //buttonBoard.button(0).onTrue(Commands.runOnce(this::toggleFunction)); // Toggle
+        //buttonBoard.button(0).onChange(Commands.runOnce(this::toggleFunction));
         
         //buttonBoard.button(0).whileTrue(Commands.either(null, null, this::getFunction));
     }
--- src/main/java/frc/robot/subsystems/arm/Wrist.java
+++ src/main/java/frc/robot/subsystems/arm/Wrist.java
@@ -4,6 +4,7 @@ import com.revrobotics.spark.SparkAbsoluteEncoder;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkAbsoluteEncoder;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
